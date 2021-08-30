02092d44c52d401bca922a5cd60234a747c8037b
Nick Gundobin <ngundobin@gmail.com>
2021-08-30 15:35:20 -0400

    added utility function to add quatities of items to the cart

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,23 @@
+/*
+	1. pass object to the function and newItem
+	2. find item if its already in the card
+	3. otherwise add item and create default value for quantity
+*/
+
+export const addItemToCart = (items, newItem) => {
+	const isItemExists = items.find((item) => item.id === newItem.id);
+	if (isItemExists) {
+		return items.map((item) => {
+			if (item.id === newItem.id) {
+				return {
+					...item,
+					quantity: newItem.quantity + 1,
+				};
+			} else {
+				return item;
+			}
+		});
+	} else {
+		return [...items, { ...newItem, quantity: 1 }];
+	}
+};
