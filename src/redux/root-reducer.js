abb3f33141b4adb6477aaf4cbfbfb7956169956b
Nick Gundobin <ngundobin@gmail.com>
2021-09-02 18:07:55 -0400

    added persistence for cart with PersistGate

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from "redux";
-
+import { persistReducer } from "redux-persist";
+// localStorage library
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
+	// user will be handled by  firebase storage, no need to persist it to localStorage
 	user: userReducer,
+	// only persist 'cart' to whitelist
 	cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 // handy to see which actions are fired etc
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// new provider will wrap our application
+export const persistor = persistStore(store);
